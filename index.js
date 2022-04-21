809ed9143937592f6954041dba039cf84af02619
begopla <begonapr@gmail.com>
2022-04-21 23:21:32 +0400

    Solved lab

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor(items,length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+     this.items.push(item);
+     this.length = this.items.length;
+     this.items.sort((a,b)=>a-b)
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(!this.items[pos]){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if(!this.length ){
+      throw new Error('EmptySortedList')
+    }
+    else return this.items[this.length-1]
+  }
 
-  min() {}
+  min() {
+    if(!this.length ){
+      throw new Error('EmptySortedList')
+    }
+    else return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+//  let totalValue = 0;
+//  this.items.forEach(num =>{
+//    totalValue += num;
+//  })
+//  return totalValue;
 
-  avg() {}
+//!using reduce:
+return this.items.reduce((acc,value)=>acc+value,0)
+
+  }
+
+  avg() {
+    if(!this.length ){
+      throw new Error('EmptySortedList')
+    }
+    else {
+      let averageValue = this.sum()/this.length;
+      return averageValue;
+    }
+  }
 }
 
 module.exports = SortedList;
